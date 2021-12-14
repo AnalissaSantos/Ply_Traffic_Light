92f83b7e82e0ba3db0910f6e8d0a55d94b844395
Analissa Santos <analissa2430@gmail.com>
2021-12-14 00:17:49 -0500

    Primer Commit Wee

---
+++ README.md
@@ -0,0 +1,20 @@
+# Semáforo con Py
+
+## Posibles Funciones que podemos hacer que haga el semáforo:
+* Cambiar Luz
+``` 
+ejemplo:
+    Rojo a Verde
+    Verde a Amarillo
+    Amarillo a Rojo
+```
+* Set intervalos de tiempo para una luz
+``` 
+ejemplo:
+    10 SEGUNDOS VERDE
+```
+  
+## Ideas para ser Extra
+* GUI
+* Intermitentes
+
---
+++ Semaforo.py
@@ -0,0 +1,59 @@
+import ply.lex as lex
+import ply.yacc as yacc
+import sys
+
+#Tokenizing
+tokens=[
+    'COLOR',
+    'CAMBIO',
+    'Tiempo'
+]
+
+#LEXER
+t_CAMBIO = r'A'
+t_ignore = r' '
+t_COLOR = r'ROJO|VERDE|AMARILLO'
+    
+
+def t_error(t):
+    print("Illegal Characters")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+
+#PARSING
+def p_cambio(i):
+    '''
+    cambio  :  expression
+            | empty
+    '''
+    print (i[1])
+
+def p_empty(i):
+    '''
+    empty :
+    '''
+    i[0]= None
+
+def p_expression(i):
+    '''
+    expression : COLOR
+    '''
+    i[0]= i[1]
+
+parser = yacc.yacc()   
+
+while True:
+    try:
+        Input= input('')
+    except EOFError:
+            break
+
+
+#lexer.input("ROJO A VERDE")
+#while True:
+#   tok = lexer.token()
+#    if not tok:
+#        break
+#    print(tok)
