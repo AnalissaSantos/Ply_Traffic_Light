8e5604577b58690ad7c3e94b5a91b0b0a67548f3
Ana Saa <anagsaam@gmail.com>
2021-12-15 17:07:28 -0500

    nuevos cambios

--- Semaforo.py
+++ Semaforo.py
@@ -8,6 +8,7 @@ tokens=[
     'COLOR',
     'CAMBIO',
     'Tiempo'
+
 ]
 
 #LEXER
@@ -15,12 +16,16 @@ t_CAMBIO = r'A'
 t_ignore = r' '
 t_COLOR = r'ROJO|VERDE|AMARILLO'
     
+# def t_tiempo(t):
+#     r'\d+'
+#     t.value = int(t.value)
+#     return t
 
 def t_error(t):
     print("Illegal Characters")
     t.lexer.skip(1)
 
-lexer = lex.lex()
+lexer = lex.lex()   #creación del lexer
 
 
 
@@ -43,6 +48,7 @@ def p_expression(i):
     expression : expression CAMBIO expression
     '''
     i[0]= (i[1],i[2],i[3])
+
 def p_expression_color(i):
     '''
     expression : COLOR
@@ -52,27 +58,28 @@ def p_expression_color(i):
 def p_error(i):
     print("Syntax Error")
 
-parser = yacc.yacc() 
+parser = yacc.yacc() #construcción del parser
 
 def run(i):
-    time.sleep(6)
-    print('Nuevo estado: '+i[2])
-    
-
-
-def run(i):
-    # if type(i)==tuple:
-    #     print ('Cambio de Luz de '+ str(run(i[0]))+ ' a'+ str(run(i[2])))
-    
-    if i[2] == 'VERDE':
-        print('Luz actual es de color '+ str(i[0]))   
-    else: print('Luz actual es de color '+ str(i[0]))
-    
     t_end = time.time() + 5
-    while time.time() < t_end:
-        time.sleep(1)
-        print('.')
-    print('NUEVA LUZ: '+ str(i[2]))
+    t_cambio = time.time() + 10
+    if (i[0] == 'VERDE') & (i[2] == 'ROJO'):
+        print('Luz actual es de color '+ str(i[0]))
+        while time.time() < t_end:
+            time.sleep(1)
+            print('.')
+        print('lUZ DE ADVERTENCIA: AMARILLO')
+        while time.time() < t_cambio:
+            time.sleep(1)
+            print('.')
+        print('NUEVA LUZ: '+ str(i[2]))
+
+    else: 
+        print('Luz actual es de color '+ str(i[0]))
+        while time.time() < t_end:
+            time.sleep(1)
+            print('.')
+        print('NUEVA LUZ: '+ str(i[2]))
 
 while True:
     try:
@@ -81,10 +88,3 @@ while True:
         break
     parser.parse(s)
 
-
-#lexer.input("ROJO A VERDE")
-#while True:
-#   tok = lexer.token()
-#    if not tok:
-#        break
-#    print(tok)
